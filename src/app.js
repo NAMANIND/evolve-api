b929de3cad094cfc0ca1b76a5fd619cc15ff9381
Naman rai <namanrai309@gmail.com>
2024-11-28 18:09:44 +0530

    update

--- src/app.js
+++ src/app.js
@@ -21,5 +21,8 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/exercises", exerciseRoutes);
 app.use("/api/videos", videoRoutes);
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
